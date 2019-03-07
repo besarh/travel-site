2d583682bd7c7c9358289d0531f388a72e1f3a66
besarh <besarhuseni@gmail.com>
2019-03-07 15:39:34 +0100

    lesson15-07march2019

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+ var path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: './app/assets/scripts/App.js',
+    output: {
+        path: path.resolve(__dirname, './app/temp/scripts'),
+        filename: 'App.js'
+
+
+    } ,
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ["env"]
+                },
+                test: /\.js$/,
+                exclude:/node_modules/
+            }
+        ]
+    }
+}
+
